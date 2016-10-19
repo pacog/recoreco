3be792b38dc016dd61cd03fcb8106bcf89a33d5e
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-10-19 11:32:42 +0200

    Simplifying arrow functions, refactoring reco list

--- app/actions.js
+++ app/actions.js
@@ -1,10 +1,8 @@
 let nextRecoId = 0;
 
-export const addReco = (recoName, recommender = '') => {
-  return {
-    type: 'ADD_RECO',
-    name: recoName,
-    recommender: recommender,
-    id: nextRecoId++
-  };
-};
+export const addReco = (recoName, recommender = '') => ({
+  type: 'ADD_RECO',
+  name: recoName,
+  recommender: recommender,
+  id: nextRecoId++
+});
--- app/containers/RecoList/index.js
+++ app/containers/RecoList/index.js
@@ -1,48 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {List, ListItem} from 'material-ui/List';
 import { Link } from 'react-router';
+
+import {List, ListItem} from 'material-ui/List';
 import RaisedButton from 'material-ui/RaisedButton';
 
 const RecoList = ({recos}) => {
-
   if(recos && recos.length) {
-    return (
-      <List>
-        {
-          recos.map( (reco, index) => {
-              return (
-                <ListItem
-                  key={`reco-${reco.id}`}
-                  primaryText={reco.name}
-                  secondaryText={reco.recommender}/>
-              );
-          })
-        }
-      </List>
-    );
+    return getRecoList(recos);
   } else {
-    return (
-      <div>
-        <p>No recommendations yet.</p>
-        <Link
-          to={'add'}
-        >
-          <RaisedButton
-            label={'Add new reco'}
-            primary={true}/>
-        </Link>
-      </div>
-
-    );
+    return getEmptyState();
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
+const getRecoList = (recos) => (
+  <List>
+    {
+      recos.map( (reco, index) => {
+          return (
+            <ListItem
+              key={`reco-${reco.id}`}
+              primaryText={reco.name}
+              secondaryText={reco.recommender}/>
+          );
+      })
+    }
+  </List>
+);
+
+const getEmptyState = () => (
+  <div>
+    <p>No recommendations yet.</p>
+    <Link
+      to={'add'}
+    >
+      <RaisedButton
+        label={'Add new reco'}
+        primary={true}/>
+    </Link>
+  </div>
+);
+
+const mapStateToProps = (state) => ({
     recos: state.recos
-  };
-}
+});
 
 const RecoListContainer = connect(mapStateToProps)(RecoList);
 
