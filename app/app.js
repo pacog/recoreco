18cdefe9b01f6986ece6968bd25eba7a1d9bf810
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-11-09 14:59:45 +0100

    Copying favicons in build

--- app/app.js
+++ app/app.js
@@ -15,7 +15,12 @@ import 'babel-polyfill';
 /* eslint-disable import/no-unresolved */
 // Load the manifest.json file and the .htaccess file
 import '!file?name=[name].[ext]!./manifest.json';
-import 'file?name=[name].[ext]!./.htaccess';
+import '!file?name=[name].[ext]!./android-chrome-192x192.png';
+import '!file?name=[name].[ext]!./android-chrome-512x512.png';
+import '!file?name=[name].[ext]!./favicon-32x32.png';
+import '!file?name=[name].[ext]!./favicon-16x16.png';
+import '!file?name=[name].[ext]!./apple-touch-icon.png';
+import '!file?name=[name].[ext]!./safari-pinned-tab.svg';
 /* eslint-enable import/no-unresolved */
 
 import React from 'react';
