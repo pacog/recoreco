f0f2a1985e40dca4804d2134e793cf42c5aea37c
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-11-09 17:13:01 +0100

    Debug offline plugin

--- app/app.js
+++ app/app.js
@@ -86,5 +86,23 @@ const render = () => {
 store.subscribe(render);
 render();
 
-// import { install } from 'offline-plugin/runtime';
-// install();
+import { install, applyUpdate } from 'offline-plugin/runtime';
+install({
+  onUpdating: () => {
+    console.log('SW Event:', 'onUpdating');
+  },
+  onUpdateReady: () => {
+    console.log('SW Event:', 'onUpdateReady');
+    // Tells to new SW to take control immediately
+    applyUpdate();
+  },
+  onUpdated: () => {
+    console.log('SW Event:', 'onUpdated');
+    // Reload the webpage to load into the new version
+    window.location.reload();
+  },
+
+  onUpdateFailed: () => {
+    console.log('SW Event:', 'onUpdateFailed');
+  }
+});
