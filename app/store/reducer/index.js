ba1df9a8f07729e95fb0e28a9c9679e2bfcbdb19
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-11-17 20:50:51 +0100

    More refactoring, combine reducers

--- app/store/reducer/index.js
+++ app/store/reducer/index.js
@@ -1,119 +1,12 @@
-const DEFAULT_ACTION = {
-  seen: false
-};
 
-const addReco = (state, action) => {
-  if(!action.name) {
-    return state;
-  }
-  const newReco = {
-    ...DEFAULT_ACTION,
-    id: action.id,
-    name: action.name,
-    recommender: action.recommender,
-    added: action.added
-  };
-  return {
-    ...state,
-    recos: state.recos.concat([newReco])
-  };
-};
+import { combineReducers } from 'redux';
+import recos from './recos';
+// import auth from './auth';
 
-const editReco = (state, action) => {
-  if(!action.name) {
-    return state;
-  }
-  if(typeof action.id === 'undefined') {
-    return state;
-  }
-  return {
-    ...state,
-    recos: state.recos.map((reco) => {
-      if(reco.id === action.id) {
-        return {
-          ...reco,
-          name: action.name,
-          recommender: action.recommender || ''
-        };
-      }
-      return reco;
-    })
-  };
-}
-
-const removeReco = (state, action) => {
-  return {
-    ...state,
-    recos: state.recos.filter( (reco) => reco.id !== action.id )
-  };
-};
-
-const markRecoAsSeen = (state, action) => {
-  return {
-    ...state,
-    recos: state.recos.map((reco) => {
-      if(reco.id === action.id) {
-        return {
-          ...reco,
-          seen: true
-        };
-      }
-      return reco;
-    })
-  };
-};
-
-const markRecoAsNotSeen = (state, action) => {
-  return {
-    ...state,
-    recos: state.recos.map((reco) => {
-      if(reco.id === action.id) {
-        return {
-          ...reco,
-          seen: false
-        };
-      }
-      return reco;
-    })
-  };
-};
-
-const rateReco = (state, action) => {
-  if (typeof action.rating === 'undefined') {
-    return state;
-  }
-  return {
-    ...state,
-    recos: state.recos.map((reco) => {
-      if(reco.id === action.id) {
-        return {
-          ...reco,
-          rating: action.rating
-        };
-      }
-      return reco;
-    })
-  };
-};
-
-export const mainReducer = (state, action = {}) => {
-  switch (action.type) {
-    case 'ADD_RECO':
-      return addReco(state, action);
-    case 'EDIT_RECO':
-      return editReco(state, action);
-    case 'REMOVE_RECO':
-      return removeReco(state, action);
-    case 'MARK_RECO_AS_SEEN':
-      return markRecoAsSeen(state, action);
-    case 'MARK_RECO_AS_NOT_SEEN':
-      return markRecoAsNotSeen(state, action);
-    case 'RATE_RECO':
-      return rateReco(state, action);
-    default:
-      return state;
-  }
-};
+export const mainReducer = combineReducers({
+  recos
+  // auth
+});
 
 export const getReco = (state, recoId) => {
   if(!state.recos) {
---
+++ app/store/reducer/recos.js
@@ -0,0 +1,98 @@
+const DEFAULT_RECO = {
+  seen: false
+};
+
+const addReco = (state, action) => {
+  if(!action.name) {
+    return state;
+  }
+  const newReco = {
+    ...DEFAULT_RECO,
+    id: action.id,
+    name: action.name,
+    recommender: action.recommender,
+    added: action.added
+  };
+  return state.concat([newReco]);
+};
+
+const editReco = (state, action) => {
+  if(!action.name) {
+    return state;
+  }
+  if(typeof action.id === 'undefined') {
+    return state;
+  }
+  return state.map((reco) => {
+      if(reco.id === action.id) {
+        return {
+          ...reco,
+          name: action.name,
+          recommender: action.recommender || ''
+        };
+      }
+      return reco;
+    });
+}
+
+const removeReco = (state, action) => {
+  return state.filter( (reco) => reco.id !== action.id );
+};
+
+const markRecoAsSeen = (state, action) => {
+  return state.map((reco) => {
+      if(reco.id === action.id) {
+        return {
+          ...reco,
+          seen: true
+        };
+      }
+      return reco;
+    });
+};
+
+const markRecoAsNotSeen = (state, action) => {
+  return state.map((reco) => {
+      if(reco.id === action.id) {
+        return {
+          ...reco,
+          seen: false
+        };
+      }
+      return reco;
+    });
+};
+
+const rateReco = (state, action) => {
+  if (typeof action.rating === 'undefined') {
+    return state;
+  }
+  return state.map((reco) => {
+      if(reco.id === action.id) {
+        return {
+          ...reco,
+          rating: action.rating
+        };
+      }
+      return reco;
+    });
+};
+
+export default (state = [], action = {}) => {
+  switch (action.type) {
+    case 'ADD_RECO':
+      return addReco(state, action);
+    case 'EDIT_RECO':
+      return editReco(state, action);
+    case 'REMOVE_RECO':
+      return removeReco(state, action);
+    case 'MARK_RECO_AS_SEEN':
+      return markRecoAsSeen(state, action);
+    case 'MARK_RECO_AS_NOT_SEEN':
+      return markRecoAsNotSeen(state, action);
+    case 'RATE_RECO':
+      return rateReco(state, action);
+    default:
+      return state;
+  }
+};
