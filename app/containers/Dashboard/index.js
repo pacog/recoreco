769f5e2009001909f4bfb6ec429c56eebca811cb
Paco Gomez Granados <pacogomezgranados@gmail.com>
2018-02-15 18:15:09 +0100

    Category selector in to watch list

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -6,11 +6,53 @@ import { connect } from 'react-redux';
 
 import Header from '../Header';
 import Footer from '../../components/Footer';
+import CategorySelector from '../../components/CategorySelector';
 import RecoList from '../../components/RecoList';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import { Card, CardText } from 'material-ui/Card';
 
 export class Dashboard extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  getContentPart = (recos, loading) => {
+    if(loading) {
+      return <LoadingIndicator />;
+    } else {
+      return (
+        <div>
+            <CategorySelector
+                value={this.state? this.state.category: null}
+                onChange={this.handleCategoryChange}/>
+                <Card>
+                  <CardText>
+                    <RecoList recos={this.filterRecos(recos)} />
+                  </CardText>
+                </Card>
+        </div>
+      );
+    }
+  };
+
+  handleCategoryChange = (newCategory) => {
+      if(this.state.category === newCategory) {
+          newCategory = null;
+      }
+      this.setState({ category: newCategory });
+  };
+
+  // TODO: this filter could be done in redux selector, but i don't know how to
+  // access local state from mapStateToProps
+  filterRecos = (recos) => {
+      if(!this.state.category) {
+          return recos;
+      } else {
+          return recos.filter(reco => reco.category === this.state.category);
+      }
+  };
 
   render() {
     return (
@@ -18,32 +60,19 @@ export class Dashboard extends React.Component {
         <Header
           title={'To watch'}
           />
-        { getContentPart(this.props.recos, this.props.isLoading) }
+          { this.getContentPart(this.props.recos, this.props.isLoading) }
         <Footer active={'toWatch'} />
       </div>
     );
   }
 };
 
-const getContentPart = (recos, loading) => {
-  if(loading) {
-    return <LoadingIndicator />;
-  } else {
-    return (
-      <Card>
-        <CardText>
-          <RecoList recos={recos} />
-        </CardText>
-      </Card>
-    );
-  }
-
-}
-
-const mapStateToProps = (state) => ({
-    recos: getUnseenRecos(state),
-    isLoading: isLoading(state)
-});
+const mapStateToProps = (state) => {
+    return {
+        recos: getUnseenRecos(state),
+        isLoading: isLoading(state)
+    }
+};
 
 const mapDispatchToProps = Object.assign(
   {},
